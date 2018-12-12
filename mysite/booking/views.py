cae221ed373d361b9461b53f8379863964cb7974
Scott Chiang <chiang_chun_wei@hotmail.com>
2018-12-13 00:42:35 +0800

    code refactoring for bookings

--- mysite/booking/views.py
+++ mysite/booking/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the bookings index.")
+    return HttpResponse("Hello, world. You're at the Booking index.")
